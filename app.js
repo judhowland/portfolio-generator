047ad8b1c1357c17ae229bcc2cebc7479db73424
Jud <jud.howland@gmail.com>
2020-07-26 11:08:25 -0500

    added initial inquirer npm functionality

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-const generatePage = require('./src/page-template');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
+
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
